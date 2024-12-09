cb7ed565520d08d0b224067c75d6cae5990d9118
Hannes <Johan van der Sman>
2024-12-09 18:14:21 +0100

    Extended example to use user defined error handler

--- example/server.py
+++ example/server.py
@@ -5,4 +5,4 @@ from pyapi.server import Application
 SPEC_PATH = Path(__file__).parent / "petstore.yaml"
 ENDPOINTS_MODULE = "example.pets"
 
-app = Application.from_file(SPEC_PATH, module=ENDPOINTS_MODULE, debug=True)
+app = Application.from_file(SPEC_PATH, module=ENDPOINTS_MODULE, error_handler="example.api_error.handle_error", debug=True)
